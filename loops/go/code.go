843a2e3e69222748669a1655d11422fca150ba48
jersobh <jersobh@gmail.com>
2024-11-27 09:07:58 +0000

    uncomment languages

--- loops/go/code.go
+++ loops/go/code.go
@@ -1,22 +1,26 @@
 package main
+
 import (
-    "fmt"
-    "math/rand"
-    "strconv"
-    "os"
+	"fmt"
+	"math/rand"
+	"time"
 )
 
 func main() {
-  input, e := strconv.Atoi(os.Args[1]) // Get an input number from the command line
-  if e != nil { panic(e) }
-  u := int(input)
-  r := int(rand.Intn(10000))           // Get a random number 0 <= r < 10k
-  var a[10000]int                      // Array of 10k elements initialized to 0
-  for i := 0; i < 10000; i++ {         // 10k outer loop iterations
-    for j := 0; j < 100000; j++ {      // 100k inner loop iterations, per outer loop iteration
-      a[i] = a[i] + j%u                // Simple sum
-    }
-    a[i] += r                          // Add a random value to each element in array
-  }
-  fmt.Println(a[r])                    // Print out a single element from the array
+	const n = 10000
+	const u = 100000
+	a := make([]int, n)
+
+	// Create a new random generator
+	rng := rand.New(rand.NewSource(time.Now().UnixNano()))
+	r := rng.Intn(10000) // Get a random number 0 <= r < 10k
+
+	for i := 0; i < n; i++ { // 10k outer loop iterations
+		for j := 0; j < u; j++ { // 100k inner loop iterations, per outer loop iteration
+			a[i] += j % u // Simple sum
+		}
+		a[i] += r // Add a random value to each element in array
+	}
+
+	fmt.Println(a[r]) // Print out a single element from the array
 }
