b4a1d7777b3c8838ad1fadc43209739eb5323949
Jeff Andrade <jersobh@gmail.com>
2024-11-29 11:52:00 +0000

    add a temp variable for the sum
    Creating a temporary variable to accumulate the results of j % u will improve performance as it will reduce the number of accesses to the array a[i].

--- loops/c/code.c
+++ loops/c/code.c
@@ -7,10 +7,12 @@ int main (int argc, char** argv) {
   int r = rand() % 10000;              // Get a random integer 0 <= r < 10k
   int32_t a[10000] = {0};              // Array of 10k elements initialized to 0
   for (int i = 0; i < 10000; i++) {    // 10k outer loop iterations
-    for (int j = 0; j < 100000; j++) { // 100k inner loop iterations, per outer loop iteration
-      a[i] = a[i] + j%u;               // Simple sum
+    int32_t temp_sum = 0;               // Temporary variable to hold the sum
+    for (int j = 0; j < 100000; j++) { // INNER_LOOP_SIZE inner loop iterations
+      temp_sum += j % u;                 // Calculate j % u and accumulate
     }
-    a[i] += r;                         // Add a random value to each element in array
+    a[i] += temp_sum;                    // Update the array with the accumulated sum
+    a[i] += r;                            // Add a random value to each element in array
   }
   printf("%d\n", a[r]);                // Print out a single element from the array
 }
